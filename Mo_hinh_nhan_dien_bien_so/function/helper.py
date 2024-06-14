9f878d02575f99e262eae380af036643aaa0ae56
mrCr4b <yueru6221@proton.me>
2024-06-14 22:43:24 +0700

    update model

--- Mo_hinh_nhan_dien_bien_so/function/helper.py
+++ Mo_hinh_nhan_dien_bien_so/function/helper.py
@@ -12,53 +12,55 @@ def check_point_linear(x, y, x1, y1, x2, y2):
     return(math.isclose(y_pred, y, abs_tol = 3))
 
 # detect character and number in license plate
-def read_plate(yolo_license_plate, im):
+def read_plate(yolo_license_plate, image):
     LP_type = "1"
-    results = yolo_license_plate(im)
-    bb_list = results.pandas().xyxy[0].values.tolist()
-    if len(bb_list) == 0 or len(bb_list) < 7 or len(bb_list) > 10:
-        return "unknown",
+    license_plate = ""
+    confidence = 0
+    results = yolo_license_plate(image)
+    text_list = results.pandas().xyxy[0].values.tolist()
+    if len(text_list) == 0 or len(text_list) < 7 or len(text_list) > 10:
+        return "unknown", confidence
     center_list = []
     y_mean = 0
     y_sum = 0
-    for bb in bb_list:
-        x_c = (bb[0]+bb[2])/2
-        y_c = (bb[1]+bb[3])/2
-        y_sum += y_c
-        center_list.append([x_c,y_c,bb[-1]])
+    for bb in text_list:
+        x_center = (bb[0]+bb[2])/2
+        y_center = (bb[1]+bb[3])/2
+        y_sum += y_center
+        center_list.append([x_center, y_center, bb[-1], bb[4]])
+    y_mean = int(int(y_sum) / len(text_list))
 
     # find 2 point to draw line
-    l_point = center_list[0]
-    r_point = center_list[0]
+    left_point = center_list[0]
+    right_point = center_list[0]
     for cp in center_list:
-        if cp[0] < l_point[0]:
-            l_point = cp
-        if cp[0] > r_point[0]:
-            r_point = cp
+        if cp[0] < left_point[0]:
+            left_point = cp
+        if cp[0] > right_point[0]:
+            right_point = cp
     for ct in center_list:
-        if l_point[0] != r_point[0]:
-            if (check_point_linear(ct[0], ct[1], l_point[0], l_point[1], r_point[0], r_point[1]) == False):
+        if left_point[0] != right_point[0]:
+            if (check_point_linear(ct[0], ct[1], left_point[0], left_point[1], right_point[0], right_point[1]) == False):
                 LP_type = "2"
-
-    y_mean = int(int(y_sum) / len(bb_list))
-    size = results.pandas().s
 
     # 1 line plates and 2 line plates
     line_1 = []
     line_2 = []
-    license_plate = ""
+
     if LP_type == "2":
         for c in center_list:
             if int(c[1]) > y_mean:
                 line_2.append(c)
             else:
                 line_1.append(c)
-        for l1 in sorted(line_1, key = lambda x: x[0]):
-            license_plate += str(l1[2])
-        for l2 in sorted(line_2, key = lambda x: x[0]):
-            license_plate += str(l2[2])
+        for text_1 in sorted(line_1, key = lambda x: x[0]):
+            license_plate += str(text_1[2])
+            confidence = (confidence + text_1[3])/2
+        for text_2 in sorted(line_2, key = lambda x: x[0]):
+            license_plate += str(text_2[2])
+            confidence = (confidence + text_2[3])/2
     else:
-        for l in sorted(center_list, key = lambda x: x[0]):
-            license_plate += str(l[2])
-    
-    return license_plate
+        for text in sorted(center_list, key = lambda x: x[0]):
+            license_plate += str(text[2])
+            confidence = (confidence + text[3])/2
+    return license_plate, confidence
--- Mo_hinh_nhan_dien_bien_so/webcam2.py
+++ Mo_hinh_nhan_dien_bien_so/webcam2.py
@@ -12,51 +12,19 @@ from datetime import datetime, timedelta
 from sqlalchemy.orm import declarative_base
 from flask import Flask, render_template
 import serial
+import requests
+
+#BASE = "http://127.0.0.1:5000/"
+BASE = "http://192.168.63.40:5003/"
 
 
-# Define the database URL
-#db_url = 'mysql://ulxn85vhqppynkyr:zEkEO66wKdpoiTZLwcJo@brtmsyfrdxo3ucwbxjmx-mysql.services.clever-cloud.com:3306/brtmsyfrdxo3ucwbxjmx'
-db_url = 'mysql://root:@192.168.63.40:3306/parkpal'
-
-# Create the SQLAlchemy engine
-engine = create_engine(db_url)
-
-# Create a sessionmaker
-Session = sessionmaker(bind=engine)
-
-# Create a base class for declarative class definitions
-Base = sqlalchemy.orm.declarative_base()
-
-    
-class car_plates(Base):
-    __tablename__ = 'car_plates'  # Specify the table name
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    user_id = Column(Integer, nullable=False)
-    plate_number = Column(String(50), nullable=False)
-    
-class parking(Base):
-    __tablename__ = 'parking'  # Specify the table name
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    id_user = Column(Integer, nullable=False)
-    car_number = Column(String(50), nullable=False)
-    time_in = Column(DateTime, nullable=True)
-    time_out = Column(DateTime, nullable=True)
-    image_in = Column(LargeBinary, nullable=True)
-    image_out = Column(LargeBinary, nullable=True)
-
-yolo_LP_detect = torch.hub.load('yolov5', 'custom', path='model/LP_detector_nano_61.pt', force_reload=True, source='local')
-yolo_license_plate = torch.hub.load('yolov5', 'custom', path='model/LP_ocr_nano_62.pt', force_reload=True, source='local')
+yolo_LP_detect = torch.hub.load('cam_ra', 'custom', path='model/license_plate.pt', force_reload=True, source='local')
+yolo_license_plate = torch.hub.load('cam_ra', 'custom', path='model/letters.pt', force_reload=True, source='local')
 yolo_license_plate.conf = 0.5
-
-roi_top_left1 = (0, 420)
-roi_bottom_right1 = (640, 480)
-license_plate_counts = {}
-count = 0
 
 # Kết nối đến webcam
 vid = cv2.VideoCapture("http://192.168.63.145:81/stream")
-#arduino = serial.Serial('COM8', 38400) # Adjust the port as needed
-running=False
+arduino = serial.Serial('COM6', 38400) # Adjust the port as needed
 # Đọc và hiển thị khung hình từ webcam
 while True:
     ret, frame = vid.read()
@@ -69,60 +37,40 @@ while True:
         w = int(plate[2] - plate[0]) # xmax - xmin
         h = int(plate[3] - plate[1]) # ymax - ymin
         cv2.rectangle(frame, (int(x), int(y)), (int(x) + int(w), int(y) + int(h)), color = (0,0,225), thickness = 2)
-        if (x >= roi_top_left1[0] and y + h >= roi_top_left1[1] and x + w <= roi_bottom_right1[0] and y + h <= roi_bottom_right1[1]):
-            running = True
-        if running==True:
-
-            crop_img = frame[y:y+h, x:x+w]
-            cv2.rectangle(frame, (int(plate[0]),int(plate[1])), (int(plate[2]),int(plate[3])), color = (0,0,225), thickness = 2)
-            cv2.imwrite("result/img2.jpg", crop_img)
-            rc_image = cv2.imread("result/img2.jpg")
-            lp = ""
-            for cc in range(0, 2):
-                for ct in range(0, 2):
-                    lp = helper.read_plate(yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
-                    if lp != ('unknown',):
-                        count += 1
-                        if lp in license_plate_counts:
-                            license_plate_counts[lp] += 1
-                        else:
-                            license_plate_counts[lp] = 1
-                        cv2.putText(frame, f"{lp}", (int(plate[0]), int(plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-                        flag = 1
-                        break
-                if flag == 1:
-                    break
-            if count >= 50:
-                with open('result/img2.jpg', 'rb') as file:
-                    image_data = file.read()
-                last_lp = max(license_plate_counts, key=license_plate_counts.get)
-                print(last_lp)
-                license_plate_counts.clear()
-                count = 0.
-                running = False
-                
-                # DÀNH CHO CAM RA
-                # Create a session
-                session = Session()
-                
-                #co_ton_tai_bien_so = session.query(bien_so).filter_by(bien_so=last_lp).first()
-                
-                da_co_trong_bang_do_xe_chua = session.query(parking).filter_by(car_number=last_lp).filter(parking.time_out.is_(None)).first()
+        crop_img = frame[y:y+h, x:x+w]
+        lp = {}
+        for cc in range(0, 2):
+            for ct in range(0, 2):
+                lp, confidence = helper.read_plate(yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
+                if lp != "unknown":
+                    confidence = round(confidence * 100,1)
+                    cv2.imwrite("result/img1.jpg", crop_img)
+                    with open('result/img2.jpg', 'rb') as file:
+                        image_data = file.read()
+                    print(lp, confidence)
+                    # DÀNH CHO CAM RA
+                    payload = {
+                        'bien_so': lp
+                    }
+                    #files = {'image': hinh_anh}
+                    files = {'image': ('img2.jpg', image_data, 'image/jpeg')}
+            
+                    response = requests.post(BASE + "camra", data=payload, files=files)
+                    response_data = response.json()
                     
-                if da_co_trong_bang_do_xe_chua:
-                    # Mở rào chắn lên 
-                    print('Xe nay se ra khoi bai -> Se mo barrier')
-                    # Cập nhật thời gian ra vào bảng do_xe
-                    da_co_trong_bang_do_xe_chua.time_out = datetime.utcnow() + timedelta(hours=7)
-                    da_co_trong_bang_do_xe_chua.image_out = image_data
-    
-                    # Commit the changes to the database
-                    session.commit()
-                    session.close()
-                    #arduino.write(b'1')
-
+                    if response_data is not None and 'ket_qua' in response_data:
+                        kq = response_data['ket_qua']
+                        if kq == 'xe_ra_khoi_bai_thanh_cong':
+                            # Mở rào chắn lên 
+                            print('Xe nay se ra khoi bai -> Se mo barrier')
+                            arduino.write(b'1')
+                    cv2.putText(frame, f"{lp}", (int(plate[0]), int(plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,225), 2)
+                    cv2.putText(frame, f"{confidence}%", (int(plate[0]), int(plate[1]-46)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,225), 2)
+                    flag = 1
+                    break
+            if flag == 1:
+                break
     # Hiển thị khung hình
-    cv2.rectangle(frame, roi_top_left1, roi_bottom_right1, color=(0, 255, 0), thickness=2)
     cv2.imshow('Cam ra', frame)
     
     # Dừng vòng lặp nếu nhấn phím 'q'
